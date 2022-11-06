10e507e88b9caa59112fa628d3ab9eae0ae1b4a2
Kristoffer Højelse <34659757+hojelse@users.noreply.github.com>
2022-11-06 22:31:13 +0100

    Update outdated keys

--- m2/xkeygen.sh
+++ m2/xkeygen.sh
@@ -1,5 +1,5 @@
 # Prepare
-openssl req -new -x509 -days 1 -keyout ca-key.pem -out ca-crt.pem
+openssl req -new -x509 -days 99 -keyout ca-key.pem -out ca-crt.pem
 # ca_password=qY2EvezssnBeaodsjxU
 # ca_cn=ca.localhost
 
@@ -13,11 +13,18 @@ openssl req -new -key server-key.pem -out server-csr.pem
 # ca_password=
 
 # Sign
-openssl x509 -req -days 1 -in server-csr.pem -CA ca-crt.pem -CAkey ca-key.pem -CAcreateserial -out server-crt.pem
+openssl x509 -req -days 99 -in server-csr.pem -CA ca-crt.pem -CAkey ca-key.pem -CAcreateserial -out server-crt.pem
 # ca_password=qY2EvezssnBeaodsjxU
 
 # Verify
 openssl verify -CAfile ca-crt.pem server-crt.pem
+
+mv server-crt.pem ./server/
+mv server-csr.pem ./server/
+mv server-key.pem ./server/
+cp ca-crt.pem ./server/
+cp ca-crt.srl ./server/
+cp ca-key.pem ./server/
 
 ### CLIENT ###
 # Client key
@@ -34,3 +41,10 @@ openssl x509 -req -days 1 -in client1-csr.pem -CA ca-crt.pem -CAkey ca-key.pem -
 
 # Verify
 openssl verify -CAfile ca-crt.pem client1-crt.pem
+
+mv client1-crt.pem ./client/
+mv client1-csr.pem ./client/
+mv client1-key.pem ./client/
+mv ca-crt.pem ./client/
+mv ca-crt.srl ./client/
+mv ca-key.pem ./client/
